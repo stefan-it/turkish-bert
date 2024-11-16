7a245fed061e3ceccc74eca9a24c6205769635dd
Stefan Schweter <stefan@schweter.it>
2024-11-16 01:51:21 +0100

    bench: remove no longer needed classes from UD dataset loader

--- experiments/ud_datasets.py
+++ experiments/ud_datasets.py
@@ -7,26 +7,6 @@ from flair.datasets import UniversalDependenciesCorpus
 from flair.file_utils import cached_path
 
 
-UD_TURKISH_MAPPING = {
-    "UD_Turkish-Tourism":  "tr_tourism",
-    "UD_Turkish-Penn":     "tr_penn",
-    "UD_Turkish-Kenet":    "tr_kenet",
-    "UD_Turkish-IMST":     "tr_imst",
-    "UD_Turkish-FrameNet": "tr_framenet",
-    "UD_Turkish-BOUN":     "tr_boun",
-    "UD_Turkish-Atis":     "tr_atis",
-}
-
-UD_TURKISH_REVISION_MAPPING = {
-    "UD_Turkish-Tourism":  "cf5aa18",
-    "UD_Turkish-Penn":     "99a4683",
-    "UD_Turkish-Kenet":    "6510b80",
-    "UD_Turkish-IMST":     "7aaa466",
-    "UD_Turkish-FrameNet": "0ab8f50",
-    "UD_Turkish-BOUN":     "283d255",
-    "UD_Turkish-Atis":     "b7d60ab",
-}
-
 class UD_GENERIC(UniversalDependenciesCorpus):
     def __init__(
         self,
